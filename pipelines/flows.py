6ee8d2390ed017183b305f2346483b37615e6f95
patriciacatandi <patriciabcatandi@gmail.com>
2024-07-30 11:50:29 -0300

    bugfix

--- pipelines/flows.py
+++ pipelines/flows.py
@@ -2,6 +2,5 @@
 """
 Imports all flows for every project so we can register all of them.
 """
-from pipelines.saneamento_drenagem.nivel_lamina_agua import *  #noqa
-from pipelines.saneamento_drenagem.nivel_reservatorio import *  #noqa
+from pipelines.saneamento_drenagem import *  #noqa
 from pipelines.exemplo import *  # noqa
